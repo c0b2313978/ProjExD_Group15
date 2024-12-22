002120019f8b42205ee71b9e66164df5d77bb4e4
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-12-22 23:20:42 +0900

    コメントの追加

--- Pacman.py
+++ Pacman.py
@@ -240,7 +240,7 @@ class Score:
 
     def update(self, screen: pg.Surface):  
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
-        screen.blit(self.image, self.rect)  
+        screen.blit(self.image, self.rect)  # スコアの描画
 
 
 def draw_start_screen(screen):
